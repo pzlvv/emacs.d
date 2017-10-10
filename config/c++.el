32e61f8f93a6c2eea4b965c027ea9d43b602bf7c
peizheng <peizheng@huawei.com>
2017-10-10 11:38:33 +0800

    Add cscope

--- config/c++.el
+++ config/c++.el
@@ -1,9 +1,20 @@
 (require 'ansi-color)
+(require 'semantic/ia)
+(require 'semantic/bovine/gcc)
+
+(custom-set-variables
+ '(company-clang-arguments '("-std=c++14")))
+
 (defun colorize-compilation-buffer ()
   (toggle-read-only)
   (ansi-color-apply-on-region compilation-filter-start (point))
   (toggle-read-only))
-(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
 
-(custom-set-variables
- '(company-clang-arguments '("-std=c++14")))
+(defun my/c++-mode-hook ()
+  )
+
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+(add-hook 'c++-mode-hook 'my/c++-mode-hook)
+
+(require 'xcscope)
+(cscope-setup)
--- config/python.el
+++ config/python.el
@@ -2,3 +2,12 @@
   (add-to-list 'company-backends 'company-jedi))
 
 (add-hook 'python-mode-hook 'my/python-mode-hook)
+
+;;pdb setup, note the python version
+(setq pdb-path '/usr/lib/python3.6/pdb.py
+      gud-pdb-command-name (symbol-name pdb-path))
+(defadvice pdb (before gud-query-cmdline activate)
+  "Provide a better default command line when called interactively."
+  (interactive
+   (list (gud-query-cmdline pdb-path
+			    (file-name-nondirectory buffer-file-name)))))
