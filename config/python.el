5fba1492c367d865fa2bc20fba291069d784c0f2
peizheng <peizheng@huawei.com>
2017-11-09 11:18:16 +0800

    Rewrite elpy goto location, cancel ipython

--- config/python.el
+++ config/python.el
@@ -1,6 +1,19 @@
 (elpy-enable)
 
-(elpy-use-ipython)
+;Rewrite elpy-goto-location without recenter
+(defun elpy-goto-location (filename offset &optional other-window-p)
+  "Show FILENAME at OFFSET to the user.
+
+If other-window-p is non-nil, show the same in other window."
+  (ring-insert find-tag-marker-ring (point-marker))
+  (let ((buffer (find-file-noselect filename)))
+    (if other-window-p
+        (pop-to-buffer buffer t)
+      (switch-to-buffer buffer))
+    (goto-char (1+ offset))
+    ))
+
+
 ;;pdb setup, note the python version
 (setq pdb-path '/usr/lib/python3.6/pdb.py
       gud-pdb-command-name (symbol-name pdb-path))
