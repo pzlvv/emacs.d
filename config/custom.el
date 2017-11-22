27e1680f99c70c917b8e1c7ae63d2d895d822615
peizheng <peizheng@huawei.com>
2017-11-22 11:26:51 +0800

    Add prototext configuration

--- config/custom.el
+++ config/custom.el
@@ -1,4 +1,5 @@
-(global-set-key  [f8] (lambda () (interactive) (find-file "E:/notes/note.org")))
-(global-set-key  [f7] (lambda () (interactive) (find-file "E:/notes/todo.org")))
+(global-set-key  [f8] (lambda () (interactive) (find-file "~/.notes/note.org")))
+(global-set-key  [f7] (lambda () (interactive) (find-file "~/.notes/todo.org")))
 
 (global-set-key (kbd "C-x g") 'magit-status)
+(add-to-list 'auto-mode-alist '("\\.prototxt\\'" . conf-mode))
--- config/unknown.el
+++
@@ -1,3 +0,0 @@
-(setq max-specpdl-size 10000)
-(setq max-lisp-eval-depth 10000)
-(setq py-max-specpdl-size 10000)
